be453c90a87b246bb0c5d6d5b7cf886fa5a44891
Laxman Ghimire <ghimirelaxman2155@gmail.com>
2025-06-10 22:47:03 +0545

    ading controller

--- backend/src/Model/orderModel.js
+++ backend/src/Model/orderModel.js
@@ -1,57 +1,76 @@
 // models/Order.js
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const orderSchema = new mongoose.Schema({
+const orderSchema = new mongoose.Schema(
+  {
     user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
-    orderItems: [
-        {
-            courseTitle: { type: String, required: true },
-            price: { type: Number, required: true },
-            course: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'Course',
-                required: true,
-            },
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    courses: [
+      {
+        courseId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Course",
+          required: true,
         },
+        name: {
+          type: String,
+          required: true,
+        },
+        price: {
+          type: Number,
+          required: true,
+        },
+      },
     ],
     paymentMethod: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
-    paymentResult: {
-        id: String,
-        status: String,
-        update_time: String,
-        email_address: String,
-    },
-    totalPrice: {
-        type: Number,
-        required: true,
-        default: 0.0,
+    totalAmount: {
+      type: Number,
+      required: true,
     },
     isPaid: {
-        type: Boolean,
-        required: true,
-        default: false,
+      type: Boolean,
+      default: false,
     },
     paidAt: {
-        type: Date,
+      type: Date,
+    },
+    paymentResult: {
+      id: String,
+      status: String,
+      update_time: String,
+      email_address: String,
     },
     isActivated: {
-        type: Boolean,
-        required: true,
-        default: false,
+      type: Boolean,
+      default: false,
     },
     activatedAt: {
-        type: Date,
+      type: Date,
     },
-}, {
+  },
+  {
     timestamps: true,
-});
+  }
+);
 
-const Order = mongoose.model('Order', orderSchema);
+const Order = mongoose.model("Order", orderSchema);
+
 module.exports = Order;
