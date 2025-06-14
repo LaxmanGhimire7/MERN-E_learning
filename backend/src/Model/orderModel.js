f8527c03db6d87059834bf06dc2ee934bbaea32a
Laxman Ghimire <ghimirelaxman2155@gmail.com>
2025-06-14 21:50:16 +0545

    order

---
+++ backend/src/Model/orderModel.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+const orderSchema = mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+    courseId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "course",
+      required: true,
+      unique: false, // You may check uniqueness in logic to prevent re-purchase
+    },
+    paymentStatus: {
+      type: String,
+      enum: ["COMPLETE", "PENDING", "CANCELED", "FULL_REFUND"],
+      default: "PENDING",
+    },
+    enrollmentStatus: {
+      type: String,
+      enum: ["ACTIVE", "EXPIRED", "CANCELED"],
+      default: "ACTIVE",
+    },
+    paymentMethod: {
+      type: String,
+      enum: ["eSewa", "Khalti", "Stripe", "PayPal"],
+      required: true,
+    },
+    invoiceId: {
+      type: String,
+      default: null,
+    },
+    paidAmount: {
+      type: Number,
+      required: true,
+    },
+    enrolledAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+
+const Order = mongoose.model("order", orderSchema);
+module.exports = Order;
