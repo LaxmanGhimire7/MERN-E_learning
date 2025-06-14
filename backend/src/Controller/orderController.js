81c28e7ce426ba5e040db97c47ddf7cb58a05cec
Laxman Ghimire <ghimirelaxman2155@gmail.com>
2025-06-14 23:11:44 +0545

    orderController

--- backend/src/Controller/orderController.js
+++ backend/src/Controller/orderController.js
@@ -1,9 +1,29 @@
 const Order = require("../Model/orderModel");
 
-const createOrder = (req, res)=>{
-    const userId = req.user._id;
-    console.log(userId)
+
+const createOrder = async (req, res) => {
+  try {
+    const { courseId, paymentMethod, paidAmount } = req.body;
+
+    const newOrder = new Order({
+      userId: req.user._id,
+      courseId,
+      paymentMethod,
+      paidAmount,
+    });
+
+    const savedOrder = await newOrder.save(); 
+    res.status(201).json({ message: "Order placed successfully", order: savedOrder });
+  } catch (error) {
+    console.error("Error saving order:", error);
+    res.status(500).json({ message: "Order failed", error });
+  }
+};
+
+const getUserOrder = async (req, res)=>{
+let userId = req.user._id;
+
 }
 
 
-module.exports={createOrder}
+module.exports={createOrder, getUserOrder}
